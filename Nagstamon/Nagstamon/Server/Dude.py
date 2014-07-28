7aa399ddec9f48fbcee2c8ccebf9ad9fb05ee512
gquadro <gquadro@operamail.com>
2014-07-28 14:02:29 +0200

    Added services

--- Nagstamon/Nagstamon/Server/Dude.py
+++ Nagstamon/Nagstamon/Server/Dude.py
@@ -63,17 +63,62 @@ class DudeServer(GenericServer):
                     n["deviceinfo_url"] = 'xxxxx'
 
                 new_host = n["host"]
-                self.new_hosts[new_host] = GenericHost()
-                self.new_hosts[new_host].name = n["host"]
-                self.new_hosts[new_host].status = 'DOWN'
-                self.new_hosts[new_host].last_check = 'n/a'
-                self.new_hosts[new_host].duration = 'n/a'
-                self.new_hosts[new_host].attempt = 'n/a'
-                self.new_hosts[new_host].status_information = n["map"] 
-                self.new_hosts[new_host].site = 'n/a'
-                self.new_hosts[new_host].deviceinfo_url = n["deviceinfo_url"]
-                #self.new_hosts[new_host].address = n["address"]
-        
+                if not self.new_hosts.has_key(n["host"]):
+                    self.new_hosts[new_host] = GenericHost()
+                    self.new_hosts[new_host].name = n["host"]
+                    self.new_hosts[new_host].status = 'DOWN'
+                    self.new_hosts[new_host].last_check = 'n/a'
+                    self.new_hosts[new_host].duration = 'n/a'
+                    self.new_hosts[new_host].attempt = 'n/a'
+                    self.new_hosts[new_host].status_information = n["map"] 
+                    self.new_hosts[new_host].site = 'n/a'
+                    self.new_hosts[new_host].deviceinfo_url = n["deviceinfo_url"]
+                    #self.new_hosts[new_host].address = n["address"]
+
+        result = self.FetchURL(self.monitor_url+'/dude/main.html?page=services')
+        tables = result.result('table', {'class': 'tbl'}) 
+        if len(tables) == 0 :
+          result = self.FetchURL(self.monitor_url+'/dude/main.html', giveback="raw", cgi_data=cgi_data)
+          result = self.FetchURL(self.monitor_url+'/dude/main.html?page=services')
+          tables = result.result('table', {'class': 'tbl'}) 
+
+        table = tables[0]
+        if len(table('tbody')) == 0 :
+            trs = table('tr', {'style': 'background: #ffc0c0;'}, recursive=False)
+        else :
+            tbody = table('tbody')[0]
+            trs = tbody('tr', recursive=False)
+        trs.pop(0)
+        for tr in trs :
+            if len(tr('td', recursive=False)) > 1 :
+                n = dict()
+                tds = tr('td', recursive=False)
+
+                n["host"] = str(tds[2].a.string)
+                n["service"] = str(tds[3].a.string)
+
+                new_host = n["host"]
+                if not self.new_hosts.has_key(n["host"]):
+                    self.new_hosts[new_host] = GenericHost()
+                    self.new_hosts[new_host].name = n["host"]
+                    self.new_hosts[new_host].status = 'UP'
+                if not self.new_hosts[new_host].services.has_key(n["service"]):
+                    self.new_hosts[new_host].services[n['service']] = GenericService()
+                    self.new_hosts[new_host].services[n['service']].host = n["host"]
+                    self.new_hosts[new_host].services[n['service']].name = n["service"]
+                    self.new_hosts[new_host].services[n['service']].server = self.name
+                    self.new_hosts[new_host].services[n['service']].status = 'CRITICAL'
+                    self.new_hosts[new_host].services[n['service']].attempt = 'n/a'
+                    self.new_hosts[new_host].services[n['service']].last_check = 'n/a'
+                    self.new_hosts[new_host].services[n['service']].duration = 'n/a'
+                    self.new_hosts[new_host].services[n['service']].status_information = 'n/a'
+                    self.new_hosts[new_host].services[n['service']].passiveonly = False
+                    self.new_hosts[new_host].services[n['service']].notifications_disabled = False
+                    self.new_hosts[new_host].services[n['service']].flapping = False
+                    self.new_hosts[new_host].services[n['service']].acknowledged = False 
+                    self.new_hosts[new_host].services[n['service']].scheduled_downtime = False
+                    self.new_hosts[new_host].services[n['service']].status_type = 'n/a'
+
         self.isChecking = False
         return Result()
 
